d70480cecad47af4b86cb33ca6cbc5b18bfdb935
Fooyer <xneerd157@gmail.com>
2023-10-30 22:42:21 -0300

    T1 #2

--- DAD2/T1/main.py
+++ DAD2/T1/main.py
@@ -1,21 +1,19 @@
 import time
 
+# Word to ASCII conversion function (unchanged)
 def word_to_ascii_list(word):
     return [ord(char) for char in word]
 
+# Counting sort for words (unchanged)
 def counting_sort_for_words(words):
-    # Convertendo cada palavra em uma lista de valores ASCII
     ascii_values = [word_to_ascii_list(word) for word in words]
-    
-    # Ordenando as palavras pelo valor ASCII de seus caracteres
     sorted_words = [word for _, word in sorted(zip(ascii_values, words))]
-    
     return sorted_words
 
+# Counting sort (unchanged)
 def counting_sort(arr):
     start_time = time.time()
     
-    # Se o primeiro item da lista é uma string, usamos o counting_sort_for_words
     if isinstance(arr[0], str):
         sorted_values = counting_sort_for_words(arr)
     else:
@@ -35,27 +33,48 @@ def counting_sort(arr):
     end_time = time.time()
     return sorted_values, end_time - start_time
 
-def read_and_sort(filename):
+# Quick sort implementation
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+def read_and_sort_with_prints(filename):
     with open(filename, 'r') as file:
         values = [line.strip() for line in file]
     
     try:
-        # Tentando converter os valores para inteiros
         values = [int(val) for val in values]
     except ValueError:
-        # Se não conseguir, os valores são tratados como strings
         pass
     
-    sorted_values, elapsed_time = counting_sort(values)
+    # Counting Sort
+    sorted_values_counting, elapsed_time_counting = counting_sort(values.copy())
+    print(f"File {filename} sorted using Counting Sort in {elapsed_time_counting:.5f} seconds!")
+    
+    # QuickSort
+    start_time = time.time()
+    sorted_values_quick = quick_sort(values.copy())
+    elapsed_time_quick = time.time() - start_time
+    print(f"File {filename} sorted using QuickSort in {elapsed_time_quick:.5f} seconds!")
+    print("-" * 50)
     
     with open(filename, 'w') as file:
-        for value in sorted_values:
+        for value in sorted_values_counting:
             file.write(str(value) + '\n')
-    
-    print(f"Arquivo {filename} ordenado em {elapsed_time:.5f} segundos!")
 
-# Lista de arquivos
+
+# List of filenames (unchanged)
 filenames = ["1000valores.txt", "10000valores.txt", "100000valores.txt", "1000words.txt", "10000words.txt", "100000words.txt"]
 
+# Note: Since I don't have access to these files, I can't run the function. But this should give you the idea.
+# Please run the following lines in your local environment:
 for filename in filenames:
-    read_and_sort(filename)
+     read_and_sort_with_prints(filename)
+
+# Return the modified functions for review
+"Functions modified and added successfully!"
